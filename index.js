015cf4d09645b5b0359c5a386683a2722ad0ff08
Fayez-fyz <zeyaffayez007@gmail.com>
2021-09-05 15:20:33 +0530

    intial-commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongodb = require('mongodb')
+const mongoClient =  mongodb.MongoClient;
+const url = 'mongodb://localhost:27017/';
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// let tasks = [];
+
+app.get("/list-all-todo",async function (req, res) {
+  try {
+        //connect the database
+        let client = await mongoClient.connect(url)
+        //select the db
+        let db = client.db('todo_app')
+        //select the connection perform the action
+        let data = await db.collection('tasks').find({}).toArray();
+        // close the connection
+        await client.close();
+      res.json(data);
+  } catch (error) {
+    res.status(500).json({
+      message:'Something went wrong'
+    })
+  }
+});
+
+app.post("/create-task", async function (req, res) {
+  try {
+      //connect the database
+     let client = await mongoClient.connect(url)
+     //select the db
+     let db = client.db('todo_app')
+     //select the connection perform the action
+     let data = await db.collection('tasks').insertOne(req.body)
+     // close the connection
+     await client.close();
+
+     res.json({
+      message: "sucessfully created",
+    });
+  } catch (error) {
+    res.status(500).status({
+      message:'Something went wrong'
+    })
+    
+  }
+
+});
+
+app.put("/update-task/:id", async function (req, res) {
+
+  try {
+      //connect the database
+      let client = await mongoClient.connect(url)
+      //select the db
+      let db = client.db('todo_app')
+      //select the connection perform the action
+         let data = await db.collection('tasks').findOneAndUpdate({_id:mongodb.ObjectId(req.params.id)},{$set:req.body})
+          // close the connection
+          await client.close();
+          res.json({
+           message: "sucessfully updated",
+         });
+  } catch (error) {
+    res.status(500).status({
+      message:'Something went wrong'
+    })
+  }
+    
+});
+
+app.delete("/delete-task/:id",async function (req, res) {
+  try {
+    //connect the database
+    let client = await mongoClient.connect(url)
+    //select the db
+    let db = client.db('todo_app')
+    //select the connection perform the action
+       let data = await db.collection('tasks').findOneAndDelete({_id:mongodb.ObjectId(req.params.id)})
+        // close the connection
+        await client.close();
+        res.json({
+         message: "sucessfully deleted",
+       });
+} catch (error) {
+  res.status(500).status({
+    message:'Something went wrong'
+  })
+}
+});
+
+app.listen(3000, function () {
+  console.log("Server is started on port 3000");
+});
